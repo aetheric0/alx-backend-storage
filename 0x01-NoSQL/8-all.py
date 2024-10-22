4616b760ea62de7f0303bca7c364790c2e2b726b
Mornark <monarchofaether@gmail.com>
2024-10-22 06:12:20 -0400

    No SQL queries and pymongo

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env/python
+""" Using Pymongo client to list all documents in a collection
+"""
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """ Connects to Database `my_db` and retrieves documents
+    in collection passed as argument
+    """
+    client = MongoClient()
+    db = client.my_db
+    result = db.mongo_collection.find()
+    documents = list(result)
+    if not documents:
+        return []
+    return documents
